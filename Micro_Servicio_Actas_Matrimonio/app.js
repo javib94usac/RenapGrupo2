0ce66d53ea6b1efa21fcf4bb4f91d0220cf144b5
201114210 <abraham.elias.elias@gmail.com>
2019-12-09 11:05:09 -0600

    interface
    Interface Matrimonio

--- Micro_Servicio_Actas_Matrimonio/app.js
+++ Micro_Servicio_Actas_Matrimonio/app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var matrimonioRouter = require('./routes/matrimonio');
+  
 
 var app = express();
 
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/matrimonio", matrimonioRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
