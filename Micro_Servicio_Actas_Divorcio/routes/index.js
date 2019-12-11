44ae98c4b28f4e04336eb6818f6d10d12e394dff
Hugo Alberto Meoño Boiton <hugoaboiton@gmail.com>
2019-12-10 18:25:43 -0600

    desarrollo actas divorci creacion de vista de datos

--- Micro_Servicio_Actas_Divorcio/routes/index.js
+++ Micro_Servicio_Actas_Divorcio/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  datos=
+  {
+    titulo:"Bienvenido al Sistema de actas de defunciones",
+    mensaje:""
+  }
+  res.render('index', {datos});
 });
 
 module.exports = router;
