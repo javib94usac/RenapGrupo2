4d2a8db48d6ad36e68dc84ca1de05fc6eff8985b
enrique777elias <abraham.elias.elias@gmail.com>
2019-12-11 10:58:43 -0600

    Desarrollo: creacion de matrimonio, SE AGREGO PRUEBAS

---
+++ Micro_Servicio_Actas_Matrimonio/Tests/functions.js
@@ -0,0 +1,46 @@
+var Compro = require('../public/javascripts/Comprobaciones.js');
+
+/*const functions = {
+  comprobarDPI: (dpi) => dpi
+
+};*/
+
+
+class pruebas
+{
+
+  constructor()
+  {
+    
+  }
+  comprobarDpi(dpi1,dpi2,)
+  {
+    var compo=new Compro(dpi1,dpi2,"");
+    if(compo. get_dpi_validos())
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  comprobarVacios(dpi1,dpi2,fecha){
+    var comprobar= new Compro(dpi1,dpi2,fecha);
+
+    if (comprobar.get_vacio()){
+
+        return true;
+
+    }else{
+
+        return false;
+
+    }
+
+
+
+  }
+}
+
+module.exports = pruebas;
