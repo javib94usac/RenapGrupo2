efa5af68e94792739ae57955bcc25c78aa79d0a7
Hugo Alberto Meoño Boiton <hugoaboiton@gmail.com>
2019-12-13 14:21:52 -0600

    corrigiendo prueba de caja blanca

--- Micro_Servicio_Actas_Divorcio/Test/functions.test.js
+++ Micro_Servicio_Actas_Divorcio/Test/functions.test.js
@@ -15,12 +15,12 @@ test ('comprobar que los campos no venga vacios',()=>{
 /*
     comprobar que los compos no vengan bien
 */
-test ('comprobar que dpi sea valido',()=>{
+test ('comprobar que dpi no sea valido',()=>{
     var prueba=new pruebas();
     expect(prueba.comprobarDpi("1234","1234")).toBeFalsy();
 });
 
-test ('comprobar que los campos no venga vacios',()=>{
+test ('comprobar que los campos  venga vacios',()=>{
    var prueba=new pruebas();
    expect(prueba.comprobarCamposNoVenganBacios("","1234567890123","")).toBeFalsy();
 });
