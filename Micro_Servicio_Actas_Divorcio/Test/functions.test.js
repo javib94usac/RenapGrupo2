50ecf2836d088f83d85aa1683a0a58703df7a935
Hugo Alberto Meoño Boiton <hugoaboiton@gmail.com>
2019-12-13 14:59:26 -0600

    agregando micro servicio divorcio

---
+++ Micro_Servicio_Actas_Divorcio/Test/functions.test.js
@@ -0,0 +1,26 @@
+var pruebas =require('./functions');
+
+/*
+    comprobar que los campos vengan bien
+*/
+test ('comprobar que dpi sea valido',()=>{
+     var prueba=new pruebas();
+     expect(prueba.comprobarDpi("1234567890123","1234567890123")).toBeTruthy();
+});
+
+test ('comprobar que los campos no venga vacios',()=>{
+    var prueba=new pruebas();
+    expect(prueba.comprobarCamposNoVenganBacios("1234567890123","1234567890123","d")).toBeTruthy();
+});
+/*
+    comprobar que los compos no vengan bien
+*/
+test ('comprobar que dpi no sea valido',()=>{
+    var prueba=new pruebas();
+    expect(prueba.comprobarDpi("1234","1234")).toBeFalsy();
+});
+
+test ('comprobar que los campos  venga vacios',()=>{
+   var prueba=new pruebas();
+   expect(prueba.comprobarCamposNoVenganBacios("","1234567890123","")).toBeFalsy();
+});
